63efcd61d9ac8ad1153462036b6bacbe94faf826
梁月 <yuhan.wyh@alibaba-inc.com>
2018-06-23 23:43:26 +0800

    fix(TS Declaration): fix declareatin

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,7 @@
 
 declare module "fresh-mysql" {
 
-    class Db {
+    export class Db {
         init( options: any, log: any ): void;
         query( sql: string, where?: Array<any>, callback?: ( error: Error|null, data: any ) => void ): void;
         transactionQuery( sqlList: Array< { sql: string, where?: Array<any>, cb?: () => {} } >, callback?: ( error: Error|null, data: any ) => void ): void;
