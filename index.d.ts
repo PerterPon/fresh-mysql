e756b3d2e40176ea2f85e0d3fb30dd952895dafa
Pon <PerterPon@gmail.com>
2019-02-21 19:46:20 +0800

    feat(declare file): add escape method

--- index.d.ts
+++ index.d.ts
@@ -2,6 +2,7 @@
 declare module "fresh-mysql" {
 
     export default class Db {
+        escape(...args: any[]): any;
         init( options: any, log: any ): void;
         query( sql: string, where?: Array<any>, callback?: ( error: Error|null, data: any ) => void ): void;
         transactionQuery( sqlList: Array< { sql: string, where?: Array<any>, cb?: () => {} } >, callback?: ( error: Error|null, data: any ) => void ): void;
